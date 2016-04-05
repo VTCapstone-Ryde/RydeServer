653d4c8b3799d3b25180c52e1a2cdf3367aa8f38
Matt McShea <mcsheam@vt.edu>
2016-04-04 21:18:26 -0400

    Changed create_group page to match designs from moqups. Ready to be converted to xhtml.

---
+++ Ryde/actual website/create_group.js
@@ -0,0 +1,20 @@
+var data = [
+    {
+        "Name": "Friend 1",
+    },
+    {
+        "Name": "Friend 2",
+    },
+    {
+        "Name": "Friend 3",
+    },
+    {
+        "Name": "Friend 4",
+    }
+];
+
+$(function () {
+    $('#table').bootstrapTable({
+        data: data
+    });
+});
