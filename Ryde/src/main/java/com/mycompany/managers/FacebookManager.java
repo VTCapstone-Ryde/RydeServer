28bfb05913d4870dfe572533c925ef159659ca3b
Eric Leszcynski <ericlski@vt.edu>
2016-04-19 15:15:18 -0400

    Fixed some CSS issues and added skeleton of the Facebook Manager

---
+++ Ryde/src/main/java/com/mycompany/managers/FacebookManager.java
@@ -0,0 +1,15 @@
+/*
+ * Created by Eric Leszcynski on 2016.04.19  * 
+ * Copyright © 2016 Eric Leszcynski. All rights reserved. * 
+ */
+package com.mycompany.managers;
+
+/**
+ *
+ * @author Eric
+ */
+public class FacebookManager {
+    
+    
+    
+}
